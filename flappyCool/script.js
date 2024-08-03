4d4e1b61b5ca1f796261c6f97c90404b92866cd3
RaymondFH <RHousteauII@gmail.com>
2024-08-03 12:45:36 -0400

    mobile bug fix, visual updates

--- flappyCool/script.js
+++ flappyCool/script.js
@@ -4,39 +4,44 @@ var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
 
-// hole position
+var gameWidth = window.innerWidth;
+var gameHeight = window.innerHeight;
+
+
+//hole position
 hole.addEventListener('animationiteration', () => {
-    var random = Math.floor(Math.random() * (400 - 150));
+    var random = Math.floor(Math.random() * (gameHeight - 30 * gameHeight / 100));
     hole.style.top = random + "px";
     counter++;
 });
+
 
 // collision detection
 setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockRight = parseInt(window.getComputedStyle(block).getPropertyValue("right"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var characterBottom = characterTop + 30; // Assuming character height is 30px
+    var characterBottom = characterTop + (3 * gameHeight / 100); // Assuming character height is 3vh
 
     if(jumping==0){
-        character.style.top = (characterTop+3)+"px";
+        character.style.top = (characterTop + gameHeight / 200) + "px";
     }
 
-    if((characterTop > 470) || (blockRight > 350 && blockRight < 400 && (characterTop < holeTop || characterBottom > holeTop + 150))){
+    if((characterTop > gameHeight * 0.97) || (blockRight > gameWidth * 0.85 && blockRight < gameWidth * 0.95 && (characterTop < holeTop || characterBottom > holeTop + gameHeight * 0.3))){
         alert("Game over. Score: "+(counter-1));
-        character.style.top = 100 + "px";
+        character.style.top = (gameHeight * 0.2) + "px";
         counter=0;
     }
 },10);
 
-// jump 
+// jump
 function jump(){
     jumping = 1;
     let jumpCount = 0;
     var jumpInterval = setInterval(function(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if((characterTop > 6) && (jumpCount < 15)){
-            character.style.top = (characterTop-5)+"px";
+        if((characterTop > gameHeight * 0.01) && (jumpCount < 15)){
+            character.style.top = (characterTop - gameHeight / 100) + "px";
         }
         if(jumpCount > 20){
             clearInterval(jumpInterval);
